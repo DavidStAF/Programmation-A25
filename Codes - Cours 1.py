dec91cf1f578ce89872ae7364e5e4a1f13f810d1
DavidStAF <dstamant@cstj.qc.ca>
2025-08-22 15:44:49 -0400

    Add files via upload

---
+++ Codes - Cours 1.py
@@ -0,0 +1,46 @@
+1 + 2
+#%%
+1 - 5
+#%%
+100 * 0.5
+#%%
+100 + 
+#%%
+25 + a
+#%%
+a = 5
+#%%
+print(a)
+#%%
+#print(a)
+#%%
+# Calcul de l'aire d'un rectangle
+
+coté_1 = 5  # Longueur du premier côté
+coté_2 = 8  # Longueur du deuxième côté
+
+aire_rectangle = coté_1 * coté_2 # Calcul de l'aire
+
+print(aire_rectangle)
+
+#%%
+# Calcul de la circonférence d'un cercle
+
+rayon = 2
+
+pi = 3.1415
+circonférence_cercle = 2*pi*rayon # Calcul de la circonférence
+
+print(circonférence_cercle)
+
+#%%
+# Calcul du total d'une facture avec taxes
+
+prix = 35
+
+montant_TPS = 5/100 * prix
+montant_TVQ = 9.975/100 * prix
+
+total = prix + montant_TPS + montant_TVQ
+
+print(total)
